bd8dfb73cb92d4a146ad92b55a4366dee90863ce
Adam Koch <me@adamko.ch>
2021-04-18 15:26:06 -0400

    Sorting is now done via Powershell.

--- Generate-CollectionTable.ps1
+++ Generate-CollectionTable.ps1
@@ -13,14 +13,13 @@ if (Test-Path -Path ".\output.md") {
 
 Import-Module -Name "Brickset"
 $credsContent = Get-Content -Path ".\credentials.txt"
-$username = ($credsContent.Split("|"))[0]
-$password = ConvertTo-SecureString -String (($credsContent.Split("|"))[1]) -AsPlainText -Force
-$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($credsContent.Split("|"))[0], (ConvertTo-SecureString -String (($credsContent.Split("|"))[1]) -AsPlainText -Force)
 Connect-Brickset -APIKey (Get-Content -Path ".\apikey.txt") -Credential $creds | Out-Null
 
 $totalSets = 0
 $totalPieces = 0
 $totalMinifigs = 0
+$legoSets = @()
 
 Add-Content -Path ".\output.md" -Value "| Name | ID | Released | Pieces | Minifigs |"
 Add-Content -Path ".\output.md" -Value "| :---- | :----: | :----: | :----: | :----: |"
@@ -36,7 +35,17 @@ Get-BricksetSetOwned -OrderBy Name | ForEach-Object {
         $minifigs = $_.minifigs
         $totalMinifigs += $_.minifigs
     }
-    Add-Content -Path ".\output.md" -Value ("| [{0}]({1}) | {2} | {3} | {4} | {5} |" -f ($_.name, $_.bricksetURL, $_.number, $_.year, $_.pieces, $minifigs)) 
+    $legoSets += @{
+        "name"        = ($_.name.trim() -replace "â ", "- ");
+        "bricksetURL" = $_.bricksetURL;
+        "number"      = $_.number;
+        "year"        = $_.year;
+        "pieces"      = $_.pieces;
+        "minifigs"    = $_.minifigs;
+    }
 }
 
+$legoSets | Sort-Object -Property "name" | ForEach-Object {
+    Add-Content -Path ".\output.md" -Value ("| [{0}]({1}) | {2} | {3} | {4} | {5} |" -f (($_.name.trim() -replace "â ", "- "), $_.bricksetURL, $_.number, $_.year, $_.pieces, $minifigs)) 
+}
 Add-Content -Path ".\output.md" -Value ("| **Totals:** | **{0}** |  | **{1}** | **{2}** |" -f ($totalSets, $totalPieces, $totalMinifigs))
